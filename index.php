7abaf4327d876af63c18f5c0dcc992c454422be2
rochmadnf <rochmadnf@gmail.com>
2021-03-12 15:37:42 +0800

    V1.0.4: changes log:  generate second.css dari file scss dan merubah beberapa class pada index.php

--- index.php
+++ index.php
@@ -14,25 +14,28 @@
         <!-- Hero -->
         <section class="section section bg-soft pb-3 overflow-hidden z-2">
             <div class="container z-2">
-                <div class="row justify-content-center text-center pt-6">
+                <div class="row justify-content-center text-center pt-3">
                     <div class="col-lg-8 col-xl-8">
-                        <h1 class="display-2 mb-3">Monitoring Tegangan</h1>
+                        <h1 class="display-2 font-weight-light mb-2">
+                            Monitoring
+                            <span class="font-weight-bold">Tegangan</span>
+                        </h1>
                     </div>
                 </div>
             </div>
         </section>
         <section class="section section-lg">
             <div class="container">
-                <div class="row">
-                    <div class="col-12 col-lg-4 col-md-6 col-sm-10 mb-4">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-lg-4 col-md-6 col-sm-8 mb-4">
                         <!-- Icon box -->
                         <div class="card bg-primary shadow-soft text-center border-light">
                             <div class="card-header">
                                 <span class="display-4 text-uppercase">Status Baterai</span>
                             </div>
                             <div class="icon-box text-center mb-5 mb-md-0">
-                                <div class="icon icon-shape icon-xxl bg-soft shadow-soft border border-light rounded-circle mb-3">
-                                    <span class="fas fa-battery-quarter text-red-500"></span>
+                                <div class="icon icon-shape icon-2xl bg-soft shadow-soft border border-light rounded-circle mb-3">
+                                    <span class="fas fa-battery-quarter text-red-5"></span>
                                 </div>
                                 <h2 class="h5 my-3"><strong>25%</strong></h2>
                             </div>
@@ -41,14 +44,14 @@
                     </div>
 
                     <!-- s:tegangan -->
-                    <div class="col-12 col-lg-4 col-md-6 col-sm-10 mb-4">
+                    <div class="col-12 col-lg-4 col-md-6 col-sm-8 mb-4">
                         <!-- Icon box -->
                         <div class="card bg-primary shadow-soft text-center border-light">
                             <div class="card-header">
                                 <span class="display-4 text-uppercase">Tegangan</span>
                             </div>
                             <div class="icon-box text-center mb-5 mb-md-0">
-                                <div class="icon icon-shape icon-xxl bg-soft shadow-soft border border-light rounded-circle mb-3">
+                                <div class="icon icon-shape icon-2xl bg-soft shadow-soft border border-light rounded-circle mb-3">
                                     <span class="fas fa-bolt text-warning"></span>
                                 </div>
                                 <h2 class="h5 my-3"><strong>15V</strong></h2>
@@ -59,14 +62,14 @@
                     <!-- e:tegangan -->
 
                     <!-- s:lampu -->
-                    <div class="col-12 col-lg-4 col-md-6 col-sm-10 mb-4">
+                    <div class="col-12 col-lg-4 col-md-6 col-sm-8 mb-4">
                         <!-- Icon box -->
                         <div class="card bg-primary shadow-soft text-center border-light">
                             <div class="card-header">
                                 <span class="display-4 text-uppercase">Lampu</span>
                             </div>
                             <div class="icon-box text-center mb-5 mb-md-0">
-                                <div class="icon icon-shape icon-xxl bg-soft shadow-soft border border-light rounded-circle mb-3">
+                                <div class="icon icon-shape icon-2xl bg-soft shadow-soft border border-light rounded-circle mb-3">
                                     <span class="fas fa-lightbulb text-dark"></span>
                                 </div>
                                 <h2 class="h5 my-3"><strong>Padam</strong></h2>
